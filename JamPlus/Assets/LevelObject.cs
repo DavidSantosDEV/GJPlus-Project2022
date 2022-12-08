3d40ff586639c5c9b031219f0f03b26580f4d564
David Santos <39455909+DavidSantosDEV@users.noreply.github.com>
2022-12-08 18:20:41 +0000

    Small fix

---
+++ JamPlus/Assets/LevelObject.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelObject : MonoBehaviour
+{
+    [SerializeField]
+    private LevelData level;
+
+
+    private void OnMouseDown()
+    {
+        //OnClicked
+
+
+
+    }
+
+}
